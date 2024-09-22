49ae72201db80b2885ffa8f1e56cf45a0ee8c926
Ivan Leung <ivanl8866@gmail.com>
2024-09-22 16:29:02 -0700

    Add default CSS styles to Button

--- src/app/lib/ui/button.tsx
+++ src/app/lib/ui/button.tsx
@@ -3,6 +3,7 @@ import { ButtonStyleProps, ComponentTWithoutP } from "./types/common";
 import cn from "./cn";
 import { Width } from "./styles/sizing/width";
 import { ButtonStyle } from "./variants/buttonstyle";
+import { Place } from "./styles/flex/place";
 
 interface ButtonProps
   extends ComponentTWithoutP<"button", "color">,
@@ -16,6 +17,9 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
         {...props}
         className={cn(
           "active:scale-95 active:ease-in-out",
+          Place.content("center"),
+          Place.items("center"),
+          Place.self("center"),
           Width.get("fit"),
           Width.getMax(maxW || "full"),
           ButtonStyle.get(variant || "solid", size || "md", color || "default"),
